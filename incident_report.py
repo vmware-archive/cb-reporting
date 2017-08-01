b449f0362b1f78a59692a96af7b82519eb46002d
Zachary Estep <zestep@carbonblack.com>
2017-08-01 09:56:47 -0400

    Updates to template and report.py

--- incident_report.py
+++ incident_report.py
@@ -4,7 +4,9 @@ import shutil
 from datetime import datetime
 from jinja2 import Environment, FileSystemLoader
 from cbapi.response import Process, Binary, Feed
+from cbapi import errors
 import cbapi.example_helpers as cbhelper
+
 #
 # The MIT License (MIT)
 #
@@ -37,16 +39,19 @@ import cbapi.example_helpers as cbhelper
 __author__ = 'jmcfarland+zestep'
 
 
-''' helper method to write the icon for a binary '''
-
-
-def write_iconfile(outdir, binary=None):
-    if binary and binary.icon:
-        write_file(outdir, binary.md5, ".png", binary.icon)
-    else:
-        dest = os.path.join(outdir, binary.md5 + ".png")
-        src = os.path.join(outdir, "images", "default.png")
-        shutil.copy(src, dest)
+def write_iconfile(output_directory, process=None):
+    try:
+        if process and process.binary and process.binary.md5:
+            if process.binary.icon:
+                write_file(output_directory, process.binary.md5, ".png", process.binary.icon)
+            else:
+                dest = os.path.join(output_directory, process.binary.md5 + ".png")
+                src = os.path.join(output_directory, "images", "default.png")
+                shutil.copy(src, dest)
+        else:
+            pass
+    except:
+        pass
 
 
 '''helper method to write a file'''
@@ -64,31 +69,27 @@ def write_file(path, name, ext, contents=None):
 '''copy static files from 'css','fonts','js','images' '''
 
 
-def copy_static_files(thedir):
-    # append ./ to the directory unless it is already relative to the cwd
+def copy_static_files(output_directory):
     dirs_to_copy = ["css", "fonts", "js", "images"]
     for d in dirs_to_copy:
-        fromdir = os.path.join(".", d)
-        todir = os.path.join(".", thedir, d)
+        from_dir = os.path.join(".", d)
+        to_dir = os.path.join(".", output_directory, d)
         try:
-            shutil.copytree(fromdir, todir)
+            shutil.copytree(from_dir, to_dir)
         except OSError:
-            pass
-        except shutil.Error:
             pass
 
 
 ''' output the report using a jinga2 template'''
 
 
-def output_from_template(cbserver, outdir, process, writers=None,
-                         feed_hits=None, hostnames=None, filepaths=None):
-
+def output_report_from_template(cbserver, output_directory, process, writers=None,
+                                feed_hits=None, hostnames=None, filepaths=None, parents=None):
     template_vars = {"process": process,
                      "sensor": process.sensor,
                      "writers": writers if writers else [],
                      "children": [child for child in process.children],
-                     "parents": [parent for parent in process.parents],
+                     "parents": [parent for parent in parents ] if parents else [],
                      "modloads": [modload for modload in process.modloads],
                      "netconns": [nc for nc in process.netconns],
                      "regmods": [rm for rm in process.regmods],
@@ -103,88 +104,95 @@ def output_from_template(cbserver, outdir, process, writers=None,
     j2_env = Environment(loader=FileSystemLoader("."),
                          trim_blocks=True)
 
-    htmlfile = file(os.path.join(outdir, "/index.html"), 'wb')
-    htmlfile.write(j2_env.get_template("incident_report.j2").render(template_vars).encode("UTF-8"))
+    report_htmlfile = file(os.path.join(output_directory, "index.html"), 'wb')
+    report_htmlfile.write(j2_env.get_template("incident_report.j2").render(template_vars).encode("UTF-8"))
 
 
 class IncidentReportGenerator(object):
-    # constructor, DI the cbresponse api bindings object
+
     def __init__(self, cbapi):
         self.cb = cbapi
 
-    # generate a report from a target GUID
-    def generate_report(self, report_guid, verbose=False):
-        outdir = os.path.join("reports", report_guid)
-        process = self.cb.select(Process, report_guid)
+    '''generate an incident report about a target guid, optional verbose console output'''
+    def generate_report(self, guid, verbose=False):
+
+        #build the report directory path and lookup the target process by GUID
+        output_directory = os.path.join("reports", guid)
+        process = self.cb.select(Process, guid)
 
         if verbose:
+            #optionally print the process and binary information to console
             process.refresh()
             process.binary.refresh()
             print(process)
             print(process.binary)
 
+        #makedirs will attempt to create the ./reports/{guid} directory
         if process.binary is not None:
             try:
-                os.makedirs(outdir)
+                os.makedirs(output_directory)
             except OSError:
                 pass
         else:
             return
 
-        # copy the static resources (images,css,js,etc) to the report directory
-        copy_static_files(outdir)
+        # copy the static resources (images,css,js,etc) to the ./reports/{guid} directory
+        copy_static_files(output_directory)
 
-        # write our own icon
-        write_iconfile(outdir, process.binary)
+        # write the icon file for the target process
+        write_iconfile(output_directory, process)
 
-        # walk children and write icons
+        # walk children of the target process and write icons
         for child in process.children:
-            write_iconfile(outdir, child.process.binary)
+            write_iconfile(output_directory, child.process)
 
         # find processes that have written to the target process's binary
         writers = []
-        writers.extend(self.cb.select(Process).where("filemod:{} hostname:{}"
+        writers.extend(self.cb.select(Process).where("filemod:{} and hostname:{} and process_md5:*"
                                                      .format(process.path, process.hostname)))
         # and write icon files for the writers
         for writer in writers:
-            write_iconfile(outdir, writer.binary)
+            write_iconfile(output_directory, writer)
 
         # build a list of hostname/filepath facets for this process-binary-by-md5
         facets = self.cb.select(Process).where("process_md5:" + process.process_md5).facets()
         filepaths = [{"filepath": e['name'], "percentage": e['ratio']} for e in facets.get('path_full')]
         hostnames = [{'hostname': e['name'], 'percentage': e['ratio']} for e in facets.get('hostname')]
 
-        # process alliance thread intelligence feed hits + write icons for feeds
+        # process alliance threat intelligence feed hits + write icons for feeds
         feed_hits = []
-        # ToDO : provide link to relevant threat report
-        for fn in process.tags:
-            alliance_score_n = "alliance_score_{}".format(fn)
-            alliance_data_n = "alliance_data_{}".format(fn)
-            feed = self.cb.select(Feed).where("name:" + fn).first()
-            write_file(outdir, feed.name, ".png", feed.icon_small)
+        # Todo : provide link to relevant threat report
+        for feed_name in process.tags:
+            # A Process has fields like "alliance_score_tor,alliance_data_tor" for each
+            alliance_score_n = "alliance_score_{}".format(feed_name)
+            alliance_data_n = "alliance_data_{}".format(feed_name)
+            feed = self.cb.select(Feed).where("name:" + feed_name).first()
+            write_file(output_directory, feed.name, ".png", feed.icon_small)
             feed_hits.append({"feed": feed.name,
                               "url": feed.provider_url,
                               "score": getattr(process, alliance_score_n),
                               "data": getattr(process, alliance_data_n)})
 
         # walk execution tree/parents and write icons
-        for parent in process.parents:
-            write_iconfile(outdir, parent.binary)
+        parents = []
+        process.walk_parents(lambda parent, depth: parents.append(parent))
+        for parent in parents:
+            write_iconfile(output_directory, parent)
 
         # use jinga templating engine to generate a report as html
-        output_from_template(self.cb.url, outdir, process, writers, feed_hits, hostnames, filepaths)
+        output_report_from_template(self.cb.url, output_directory, process, writers, feed_hits, hostnames, filepaths, parents)
 
-        print("[+] Report generated in ./{}\n".format(outdir + "/"))
+        print("[+] Report generated in ./{}\n".format(output_directory + "/"))
 
 
 def main():
     parser = cbhelper.build_cli_parser()
-    parser.add_argument("--guid", dest="guid", help="GUID of target process", required=True)
+    parser.add_argument("--guid", dest="guid", help="GUID of target process",required=True)
     args = parser.parse_args()
-    repgen = IncidentReportGenerator(cbapi=cbhelper.get_cb_response_object(args))
+    cbapi = cbhelper.get_cb_response_object(args)
+    repgen = IncidentReportGenerator(cbapi=cbapi)
     print("[+] Generating report for process guid: {}\n".format(args.guid))
-    repgen.generate_report(report_guid=args.guid, verbose=True if args.verbose else False)
-
+    repgen.generate_report(guid=args.guid, verbose=True if args.verbose else False)
 
 if __name__ == "__main__":
     main()
